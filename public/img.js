84387caa2a52a99e7bc0f41ec7f0ebf78dce77fd
Devanshi Sharma <devanshi.ds.20@gmail.com>
2021-01-23 20:00:51 +0530

    initial commit

---
+++ public/img.js
@@ -0,0 +1,25 @@
+let photo =  document.querySelector("#photo");
+let imgUpload = document.querySelector("#photo-upload");
+let download = document.querySelector("#download");
+
+photo.addEventListener("click" , function(){
+    imgUpload.click();
+})
+
+imgUpload.addEventListener("change" , function(e){
+    let photo = e.target.files[0]; 
+    let src = URL.createObjectURL(photo);
+    let img = document.createElement("img");
+    img.setAttribute("src" , src);
+    let stickyContent = createSticky();
+    stickyContent.append(img);
+})
+
+download.addEventListener("click" , function(){
+    
+    let image = canvas.toDataURL("image/png");
+    let aTag = document.createElement("a");
+    aTag.download = "canvas.png";
+    aTag.href = image;
+    aTag.click();
+})
---
+++ public/tools.js
@@ -0,0 +1,129 @@
+let pencil = document.querySelector("#pencil");
+let pencilOptions = document.querySelector("#pencil-options");
+let eraser = document.querySelector("#eraser");
+let eraserOptions = document.querySelector("#eraser-options");
+
+let red = document.querySelector(".red");
+let blue = document.querySelector(".blue");
+let yellow = document.querySelector(".yellow");
+let black = document.querySelector(".black");
+
+let pencilSize = document.querySelector('#pencil-size');
+let eraserSize = document.querySelector('#eraser-size');
+
+let undo = document.querySelector("#undo");
+let redo = document.querySelector("#redo");
+
+
+let lastPencilSize = 1;
+let lastEraserSize = 1;
+
+pencilSize.addEventListener("change" , function(){
+    lastPencilSize = pencilSize.value;
+    ctx.lineWidth = lastPencilSize;
+})
+
+eraserSize.addEventListener("change" , function(){
+    lastEraserSize = eraserSize.value;
+    ctx.lineWidth = lastEraserSize;
+})
+
+red.addEventListener("click" , function(){
+    ctx.strokeStyle = "red";
+})
+blue.addEventListener("click" , function(){
+    ctx.strokeStyle = "blue";
+})
+yellow.addEventListener("click" , function(){
+    ctx.strokeStyle = "yellow";
+})
+black.addEventListener("click" , function(){
+    ctx.strokeStyle = "black";
+})
+
+pencil.addEventListener("click", function () {  
+  if (pencil.classList.contains("active-tool")) {
+    // pencil ke options open
+    if (pencilOptions.classList.contains("hide")) {
+        pencilOptions.classList.remove("hide");
+    } else{
+        pencilOptions.classList.add("hide");
+    }
+  } else {
+    ctx.strokeStyle = "black";
+      ctx.lineWidth = lastPencilSize;
+    // pencil active nahi
+    if (!eraserOptions.classList.contains("hide")) {
+      eraserOptions.classList.add("hide");
+    }
+    pencil.classList.add("active-tool");
+    eraser.classList.remove("active-tool");
+  }
+});
+
+eraser.addEventListener("click", function () {
+  if (eraser.classList.contains("active-tool")) {
+    // eraser ke options open
+    if (eraserOptions.classList.contains("hide")) {
+        eraserOptions.classList.remove("hide");
+    }else {
+        eraserOptions.classList.add("hide");
+    }
+  } else {
+    ctx.strokeStyle = "white";
+    ctx.lineWidth = lastEraserSize;
+    if (!pencilOptions.classList.contains("hide")) {
+      pencilOptions.classList.add("hide");
+    }
+    eraser.classList.add("active-tool");
+    pencil.classList.remove("active-tool");
+  }
+});
+
+undo.addEventListener("click" , function(){
+  let undoLine = db.pop();
+  redoDb.push(undoLine);
+  // eraseLine(undoLine);
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  redraw();
+})
+
+function redraw(){
+  for(let i=0 ; i<db.length ; i++){
+    let line = db[i];
+    for(let j=0 ; j<line.length ; j++){
+      let lineObj = line[j];
+      ctx.strokeStyle = lineObj.color;
+      ctx.lineWidth = lineObj.width;
+      if(lineObj.id == 'md'){
+        ctx.beginPath();
+        ctx.moveTo(lineObj.x , lineObj.y);
+      }
+      else{
+        ctx.lineTo(lineObj.x , lineObj.y);
+        ctx.stroke();
+      }
+    }
+  }
+}
+
+redo.addEventListener("click" , function(){
+  if(redoDb.length){
+    let redoLine = redoDb.pop();
+    db.push(redoLine);
+    for(let i=0 ; i<redoLine.length ; i++){
+      let lineObj = redoLine[i];
+      ctx.strokeStyle = lineObj.color;
+      ctx.lineWidth = lineObj.width;
+      if(lineObj.id == 'md'){
+        ctx.beginPath();
+        ctx.moveTo(lineObj.x , lineObj.y);
+      }
+      else{
+        ctx.lineTo(lineObj.x , lineObj.y);
+        ctx.stroke();
+      }
+    }
+
+  }
+})
